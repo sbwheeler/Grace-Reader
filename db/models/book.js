66eea378f2462732b137864b2e73456e938b230c
Sam Wheeler <swheeler111@gmail.com>
2017-01-09 17:23:49 -0500

    working on books and reviews models

---
+++ db/models/book.js
@@ -0,0 +1,61 @@
+const db = require('APP/db');
+const Sequelize = require('sequelize');
+const Review = require('review');
+
+const Book = db.define('books', {
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  author: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  genre: Sequelize.ARRAY(Sequelize.STRING),
+  price: {
+    type: Sequelize.FLOAT,
+    validate: {
+      allowNull: false
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    validate: {
+      notEmpty: true
+    }
+  },
+  stockCount: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: 'https://freeiconshop.com/files/edd/book-open-flat.png',
+    validate: {
+      isUrl: true
+    }
+  }
+},
+{
+  getterMethods: {
+    //pulls the average rating from reviews of the book
+    ratingAverage: function() {
+      let average;
+      Review.findAll({
+        attributes: ['rating'],
+        where: {
+          bookId: this.id
+        }
+      })
+      .then(ratings => {
+        console.log(ratings);
+      })
+
+      return average || 0;
+    }
+  }
+});
