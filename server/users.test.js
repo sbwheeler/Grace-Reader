a700bc4fb59a5eff02848af10007b3c6da1e0412
Sam Wheeler <swheeler111@gmail.com>
2017-01-10 14:57:43 -0500

    made user routes and tests

--- server/users.test.js
+++ server/users.test.js
@@ -1,38 +1,48 @@
 const request = require('supertest-as-promised')
-const {expect} = require('chai')
+const { expect } = require('chai')
 const db = require('APP/db')
 const User = require('APP/db/models/user')
 const app = require('./start')
 
-describe('/api/users', () => {
+describe.only('/api/users', () => {
   describe('when not logged in', () => {
     it('GET /:id fails 401 (Unauthorized)', () =>
       request(app)
-        .get(`/api/users/1`)
-        .expect(401)
-    )    
+      .get(`/api/users/1`)
+      .expect(401)
+    )
 
     it('POST creates a user', () =>
       request(app)
-        .post('/api/users')
-        .send({
-          email: 'beth@secrets.org',
-          password: '12345'
-        })
-        .expect(201)
+      .post('/api/users')
+      .send({
+        firstName: 'beth',
+        lastName: 'wheeler',
+        email: 'beth@secrets.org',
+        password: '12345'
+      })
+      .expect(201)
     )
 
     it('POST redirects to the user it just made', () =>
       request(app)
-        .post('/api/users')
-        .send({
-          email: 'eve@interloper.com',
-          password: '23456',
-        })
-        .redirects(1)
-        .then(res => expect(res.body).to.contain({
-          email: 'eve@interloper.com'
-        }))        
+      .post('/api/users')
+      .send({
+        firstName: 'eve',
+        lastName: 'ye',
+        email: 'eve@interloper.com',
+        password: '23456',
+      })
+      .redirects(1)
+      .then(res => expect(res.body).to.contain({
+        email: 'eve@interloper.com'
+      }))
+    )
+
+    it('DELETE /:id fails 401 (Unauthorized)', () =>
+      request(app)
+      .delete('/api/users/1')
+      .expect(401)
     )
   })
 })
