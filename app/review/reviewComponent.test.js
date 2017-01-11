c104e01d5be066d52370155b1390849925958eaf
dye784 <damon.ye91@gmail.com>
2017-01-11 15:03:58 -0500

    basic test for review component

--- app/review/reviewComponent.test.js
+++ app/review/reviewComponent.test.js
@@ -1,4 +1,16 @@
 import { expect } from 'chai';
 import React from 'react';
+import { shallow } from 'enzyme';
 
-import ReviewComponent from 'reviewComponent';
+import ReviewComponent from './reviewComponent';
+
+describe.only('Review Component', () => {
+  let review;
+  beforeEach('Create component', () => {
+    review = shallow(<ReviewComponent />)
+  })
+
+  it('should be a <div> with an expected background', () => {
+    expect(review.is('div')).to.be.true
+  })
+})
