a23192fddaf8ffe9e7550a66b94610003333641d
dye784 <damon.ye91@gmail.com>
2017-01-15 10:14:15 -0500

    mod singlebookcomp into class, button effect for on click

--- app/book/singleBookComponent.js
+++ app/book/singleBookComponent.js
@@ -5,38 +5,47 @@ import SingleBookReview from './singleBookReview'
 import StarRatingComponent from 'react-star-rating-component'
 import NewReviewForm from '../review/newReviewForm'
 
+class SingleBookComponent extends Component {
+  constructor(props) {
+    super(props)
 
-const SingleBookComponent = ({ currentBook }) => {
-  return (
-    <div className="currentBook">
-      <div>
-        <h3>{currentBook.title}</h3>
-        <div>{currentBook.author}</div>
+    this.state = { addedToCart: false }
+  }
+
+  render() {
+    const currentBook = this.props.currentBook
+    return (
+      <div className="currentBook">
+        {this.state.addedToCart && <h1>ADDED TO CART!!</h1>}
         <div>
-        <Link to={`/user/cart`}>Add to cart</Link>
-        <StarRatingComponent
-          name="rate1"
-          editing={false}
-          starCount={5}
-          value={currentBook.reviews && currentBook.reviews.map(review => review.rating).reduce((a, b) => { return a + b;}, 0) / currentBook.reviews.length}
-          renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
-        />
-        </div>
-        <img src={currentBook.imageUrl} className="img-thumbnail" />
-      </div>
-      <h3>REVIEWS</h3>
-      {
-        currentBook.reviews && currentBook.reviews.map(review => (
-          <div key={review.id}>
-            <Link to={`/reviews/${review.id}`}>
-              <SingleBookReview review={review} />
-            </Link>
+          <h3>{currentBook.title}</h3>
+          <div>{currentBook.author}</div>
+          <div>
+          <button onClick={() => this.setState({addedToCart: true})}>Add to cart</button>
+          <StarRatingComponent
+            name="rate1"
+            editing={false}
+            starCount={5}
+            value={currentBook.reviews && currentBook.reviews.map(review => review.rating).reduce((a, b) => { return a + b;}, 0) / currentBook.reviews.length}
+            renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
+          />
           </div>
-        ))
-      }
-      <NewReviewForm book={currentBook} />
-    </div>
-  )
+          <img src={currentBook.imageUrl} className="img-thumbnail" />
+        </div>
+        <h3>REVIEWS</h3>
+        {
+          currentBook.reviews && currentBook.reviews.map(review => (
+            <div key={review.id}>
+              <Link to={`/reviews/${review.id}`}>
+                <SingleBookReview review={review} />
+              </Link>
+            </div>
+          ))
+        }
+        <NewReviewForm book={currentBook} />
+      </div>
+    )
+  }
 }
 
 export default SingleBookComponent;
