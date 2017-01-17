fcac0b7d8493bd5e83d2bdd11572b759e27f3110
Sam Wheeler <swheeler111@gmail.com>
2017-01-17 13:51:33 -0500

    updated nav

--- app/book/singleBookComponent.js
+++ app/book/singleBookComponent.js
@@ -22,36 +22,40 @@ class SingleBookComponent extends Component {
   render() {
     const currentBook = this.props.currentBook
     return (
-      <div className="currentBook">
-        {this.state.addedToCart && <h1>ADDED TO CART!!</h1>}
-        <div>
-          <h3>{currentBook.title}</h3>
-          <div>by {currentBook.author}</div>
-          <div>Description : {currentBook.description}</div>
-          <div>Price: $ {currentBook.price}</div>
-        <div>
-          <button onClick={this.handleClick}>Add to cart</button>
-          <StarRatingComponent
-            name="rate1"
-            editing={false}
-            starCount={5}
-            value={currentBook.reviews && currentBook.reviews.map(review => review.rating).reduce((a, b) => { return a + b;}, 0) / currentBook.reviews.length}
-            renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
-          />
-          </div>
-          <img src={currentBook.imageUrl} className="img-thumbnail" height="300" width="200"/>
-        </div>
-        <h3>REVIEWS</h3>
-        {
-          currentBook.reviews && currentBook.reviews.map(review => (
-            <div key={review.id}>
-              <Link to={`/reviews/${review.id}`}>
-                <SingleBookReview review={review} />
-              </Link>
+      <div className="currentBook container-fluid">
+        <div class="row" >
+          <div class='col-sm-5 col-centered'>
+            {this.state.addedToCart && <h1>ADDED TO CART!!</h1>}
+            <div>
+              <h3>{currentBook.title}</h3>
+              <div>by {currentBook.author}</div>
+              <div>Description : {currentBook.description}</div>
+              <div>Price: $ {currentBook.price}</div>
+            <div>
+              <button onClick={this.handleClick}>Add to cart</button>
+              <StarRatingComponent
+                name="rate1"
+                editing={false}
+                starCount={5}
+                value={currentBook.reviews && currentBook.reviews.map(review => review.rating).reduce((a, b) => { return a + b;}, 0) / currentBook.reviews.length}
+                renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
+              />
+              </div>
+              <img src={currentBook.imageUrl} className="img-thumbnail" height="300" width="200"/>
             </div>
-          ))
-        }
-        <NewReviewContainer book={currentBook} />
+            <h3>REVIEWS</h3>
+            {
+              currentBook.reviews && currentBook.reviews.map(review => (
+                <div key={review.id}>
+                  <Link to={`/reviews/${review.id}`}>
+                    <SingleBookReview review={review} />
+                  </Link>
+                </div>
+              ))
+            }
+            <NewReviewContainer book={currentBook} />
+          </div>
+        </div>
       </div>
     )
   }
